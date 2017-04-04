9c82177656664d30ba9f2095af5a6c318fd82d6b
deargopinath <deargopinath@gmail.com>
2017-04-04 16:31:05 +0300

    Added Java object for Button Component

---
+++ aem-sample-we-retail-we.retail-1.0.0/ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/button/Button.java
@@ -0,0 +1,45 @@
+package apps.we_retail.components.content.button;
+import com.adobe.cq.sightly.WCMUse;
+import com.adobe.cq.sightly.WCMUsePojo;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.resource.ValueMap;
+import java.util.*;
+import org.apache.sling.commons.json.JSONArray;
+import org.apache.sling.commons.json.JSONObject;
+import org.apache.sling.commons.json.JSONException;
+import javax.jcr.*;
+import com.day.cq.commons.jcr.*;
+
+public class Button extends WCMUsePojo {
+
+    private ValueMap properties;
+    private Page page;
+    private PageManager pageManager;
+    private String path;
+    private String locale = "en-us";
+    String pageName;
+
+    HashMap<String,String> promoMap = new HashMap<String,String>();
+
+    @Override
+    public void activate() throws Exception {
+        this.properties = getProperties();
+        page = super.getCurrentPage();
+        pageManager = super.getPageManager();
+        path = page.getPath().toLowerCase();
+        pageName = page.getName().toLowerCase();
+    }
+
+    public String getBackgroundColor() {
+        return properties.get("backgroundColor", String.class);
+    }
+    public String getLinkTo() {
+        return properties.get("http://www.google.com", String.class);
+    }
+    public String getLabel() {
+        return properties.get("label", String.class);
+    }
+
+}
+
