32f096b33e6b979051ab9b4be7b0f00a9cc7d6c7
admin <dnagarajan89@gmail.com>
2017-04-05 11:59:57 +0300

    banner - dhanesh

--- aem-sample-we-retail-we.retail-1.0.0/ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/danbanner/Banner.java
+++ aem-sample-we-retail-we.retail-1.0.0/ui.apps/src/main/content/jcr_root/apps/we-retail/components/content/danbanner/Banner.java
@@ -1,4 +1,5 @@
 package apps.we_retail.components.content.danbanner;
+
 import com.adobe.cq.sightly.WCMUse;
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.day.cq.wcm.api.Page;
@@ -13,43 +14,46 @@ import com.day.cq.commons.jcr.*;
 
 public class Banner extends WCMUsePojo {
 
-    private ValueMap properties;
-    private Page page;
-    private PageManager pageManager;
-    private String path;
-    private String locale = "en-us";
-    String pageName;
+	private ValueMap properties;
+	private Page page;
+	private PageManager pageManager;
+	private String path;
+	private String locale = "en-us";
+	String pageName;
 
-    HashMap<String,String> promoMap = new HashMap<String,String>();
+	HashMap<String, String> promoMap = new HashMap<String, String>();
 
-    @Override
-    public void activate() throws Exception {
-        this.properties = getProperties();
-        page = super.getCurrentPage();
-        pageManager = super.getPageManager();
-        path = page.getPath().toLowerCase();
-        pageName = page.getName().toLowerCase();
-    }
+	@Override
+	public void activate() throws Exception {
+		this.properties = getProperties();
+		page = super.getCurrentPage();
+		pageManager = super.getPageManager();
+		path = page.getPath().toLowerCase();
+		pageName = page.getName().toLowerCase();
+	}
 
-    public String getHeadline() {
-        return properties.get("headline", String.class);
-    }
-    public String getBody() {
-        return properties.get("body", String.class);
-    }
-    public String getLinkLabel() {
-        return properties.get("linkLabel", String.class);
-    }
-    public String getOpenNewTab() {
-        return properties.get("openNewTab", String.class);
-    }
-     public String getImageUrl() {
-        return properties.get("imageUrl", String.class);
-    }
+	public String getHeadline() {
+		return properties.get("headline", String.class);
+	}
 
-    public String getLinkTo() {
-        return properties.get("linkTo", String.class);
-    }
+	public String getBody() {
+		return properties.get("body", String.class);
+	}
+
+	public String getLinkLabel() {
+		return properties.get("linkLabel", String.class);
+	}
+
+	public String getOpenNewTab() {
+		return properties.get("openNewTab", String.class);
+	}
+
+	public String getImageUrl() {
+		return properties.get("imageUrl", String.class);
+	}
+
+	public String getLinkTo() {
+		return properties.get("linkTo", String.class);
+	}
 
 }
-
